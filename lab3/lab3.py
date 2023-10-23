2891885602202e46a933f2043538f2a2efa4b080
G0ld3nDr4g0n3301 <ainsoph_itmo@mail.ru>
2023-10-23 09:31:48 +0300

    Added solution for the third task

---
+++ lab3/lab3.py
@@ -0,0 +1,31 @@
+import re
+
+def main(strings,letters,space):
+	reg = '[%s].{%s}[%s].{%s}[%s]' % (letters[0],space,letters[1],space,letters[2])
+	expr = re.compile(reg)
+	for string in strings:
+		sub = expr.search(string)
+		if sub == None:
+			continue
+		if string == sub.group(0):
+			yield string
+		else:
+			continue
+
+
+if __name__ == '__main__':
+	letters = [i for i in input('enter 3 letters: ')]
+	if len(letters) != 3: 
+		print('3 letters')
+		raise SystemExit
+	try:
+		space = int(input('enter the length of spaces between letters: '))
+		word_num = int(input('Enter the number of words: '))
+	except:
+		print('Enter a NUMBERS')
+		raise SystemExit
+	string = []
+	for i in range(word_num):
+		string.append(input(''))
+	for i in main(string,letters,space):
+		print(i)
