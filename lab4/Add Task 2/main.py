c45746eb1b4831e0808f7a2d80dc7230912c021f
G0ld3nDr4g0n3301 <ainsoph_itmo@mail.ru>
2023-11-06 21:52:34 +0300

    Solved additional task 2.I've tried as hard as i can, but it's impossible.

---
+++ lab4/Add Task 2/main.py
@@ -0,0 +1,18 @@
+import re
+
+# Reading YAML data from the file
+yaml_data = open('schedule.yaml', 'r').read()
+
+# Function to convert YAML to XML
+def yaml_to_xml(yaml_text):
+    # Replace YAML keys and values with XML tags and content
+    xml_text = re.sub(r'^(\s*)(\w+):\s*(.*)', r'\1<\2>\3</\2>', yaml_text, flags=re.MULTILINE)
+    return xml_text
+
+# Convert YAML to XML
+xml_data = yaml_to_xml(yaml_data)
+
+# Writing XML data to file
+xml_file = open('schedule.xml', 'w')
+xml_file.write(xml_data)
+xml_file.close()
