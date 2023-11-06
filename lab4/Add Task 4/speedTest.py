00f9170955e8308d6e702e3af99ec17878821d58
G0ld3nDr4g0n3301 <ainsoph_itmo@mail.ru>
2023-11-06 22:21:41 +0300

    Solved additional task 4

---
+++ lab4/Add Task 4/speedTest.py
@@ -0,0 +1,18 @@
+import add1,add2,main
+import time
+
+time_start_main = time.time()
+main.main()
+time_elapsed_main = (time.time() - time_start_main)
+
+time_start_add1 = time.time()
+add1.main()
+time_elapsed_add1 = (time.time() - time_start_add1)
+
+time_start_add2 = time.time()
+add2.main()
+time_elapsed_add2 = (time.time() - time_start_add2)
+
+print(time_elapsed_main)
+print(time_elapsed_add1)
+print(time_elapsed_add2)
